065a7debc643a39468d380684b1bfdfdde4bec2d
Pei-Wei <pei-wei.wu@hobsons.com>
2015-05-07 12:21:13 -0700

    django1.6

--- dajaxice/templates/dajaxice/dajaxice.core.js
+++ dajaxice/templates/dajaxice/dajaxice.core.js
@@ -41,6 +41,7 @@ var Dajaxice = {
         send_data.push('argv='+encodeURIComponent(JSON.stringify(argv)));
         send_data = send_data.join('&');
         var oXMLHttpRequest = new XMLHttpRequest;
+        oXMLHttpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         oXMLHttpRequest.open('POST', '/{{DAJAXICE_URL_PREFIX}}/'+dajaxice_function+'/');
         oXMLHttpRequest.setRequestHeader("X-Requested-With", "XMLHttpRequest");
         oXMLHttpRequest.setRequestHeader("X-CSRFToken",Dajaxice.get_cookie('csrftoken'));
